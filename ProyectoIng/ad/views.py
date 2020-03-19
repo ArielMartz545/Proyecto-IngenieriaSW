b0cabfd53aed08c4e4ec28298373da8a34328341
fabricio <fabriciomurillo24@gmail.com>
2020-03-19 01:05:15 -0600

    Cambios a Mis Anuncios y Anuncios por categoria
    Se ordenan de forma descendente.

--- ProyectoIng/ad/views.py
+++ ProyectoIng/ad/views.py
@@ -8,14 +8,12 @@ from images.models import Image
 
 
 class ShowAdsListView(ListView):
-
     model= Ad
     template_name="ad_list.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context["ads_data"]= Ad.objects.prefetch_related()
-        
+        context["ads_data"]= Ad.objects.prefetch_related().order_by('-date_created')
         return context
 #id_user ,id_store ,id_location,id_ad_kind,id_category,id_unit,ad_name,ad_description,price,date_created
 
@@ -25,5 +23,5 @@ class CategoryDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['products']= Ad.objects.all()
+        context['products']= Ad.objects.all().order_by('-date_created')
         return context
