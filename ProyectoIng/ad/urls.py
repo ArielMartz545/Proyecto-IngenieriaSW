ff86b503031fd6987023cca2f43c14602a0fb72f
Alexis <Alexismatamoros1996888@gmail.com>
2020-03-19 21:49:03 -0600

    Create Ads Done 90%

--- ProyectoIng/ad/urls.py
+++ ProyectoIng/ad/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ShowAdsListView, CategoryDetailView, AdDetailView
+from .views import ShowAdsListView, CategoryDetailView, AdDetailView, AdCreate
 
 urlpatterns = [
     path('', ShowAdsListView.as_view(), name='my_products'),
     path('category/<int:pk>', CategoryDetailView.as_view(), name="category_products"),
-    path('<int:pk>', AdDetailView.as_view(), name="ad_detail")
+    path('<int:pk>', AdDetailView.as_view(), name="ad_detail"),
+    path('createad/', AdCreate, name="ad_create")
 ]
--- ProyectoIng/ad/views.py
+++ ProyectoIng/ad/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Ad, Category, PriceRange
 from location.models import Location
 from account.models import Account
 from images.models import Image
+from ad.forms import AdCreateForm,AdImageForm
+from django.urls import reverse_lazy
 
 
 class ShowAdsListView(ListView):
@@ -33,4 +36,28 @@ class AdDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         #context['ads'] = Ad.objects.all().order_by('-date_created')
-        return context
+        return render (reverse_lazy('home'))
+
+
+
+def AdCreate(request):
+    model = Ad
+    if  request.method == "POST" :
+        ad_form= AdCreateForm(request.POST)
+        img_form= AdImageForm(request.POST,request.FILES )
+        print("img es "+ str(img_form.is_valid()))
+        print(img_form)
+        if ad_form.is_valid() and img_form.is_valid():
+            img= img_form.save()
+            ad= ad_form.save(False)
+            ad.id_user = request.user
+            ad= ad_form.save()
+            ad.ad_images.add(img)#Esta debe ser una instancia de la tabla transaccional
+            ad.save()
+            print("Almacenado")
+            return render(request,'ad/ad_list.html')
+    else:
+        ad_form=AdCreateForm()
+        img_form=AdImageForm()
+
+    return render(request,'ad/ad_create.html',{'Ad_form': ad_form,'img_form':img_form})
