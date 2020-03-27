94ec81efe904fa4afaf014c0d7458c735ebcade7
Nelson Díaz <diaznelson23@gmail.com>
2020-03-26 23:18:59 -0600

    slide custom

---
+++ ProyectoIng/core/static/core/js/slide.js
@@ -0,0 +1,30 @@
+function setThumbSize(){
+    var width = $("#img-thumb-container").width();
+    $(".img-thumb").width(width);
+    $(".img-thumb").height(width);
+}
+function setBoxSize(){
+    var width = $("#img-box-container").width();
+    $("#img-box-container").height(width);
+}
+$( ".img-thumb" ).hover(
+    function() {
+        $('.img-detail').attr('src',$( this ).attr('src'));
+    }, function() {}
+);
+$( ".img-thumb" ).hover(
+    function() {
+        $('.img-detail').attr('src',$( this ).attr('src'));
+    }, function() {}
+);
+$( ".img-thumb" ).click(function() {
+    $('.img-detail').attr('src',$( this ).attr('src'));
+});
+$( document ).ready(function() {
+    setThumbSize();
+    setBoxSize();
+});
+$( window ).resize(function() {
+    setThumbSize();
+    setBoxSize();
+});
