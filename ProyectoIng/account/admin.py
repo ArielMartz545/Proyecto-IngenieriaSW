4d21ed9dc3530ed72ffbfc1d1fa16c7cd759daa2
Alexis <Alexismatamoros1996888@gmail.com>
2020-05-04 12:00:01 -0600

    Grafos funcionales

--- ProyectoIng/account/admin.py
+++ ProyectoIng/account/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import Account,AccountManager
 
 # Register your models here.
-
-admin.site.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    readonly_fields = ('date_joined', )
+ 
+admin.site.register(Account,AccountAdmin)
--- ProyectoIng/graphs/views.py
+++ ProyectoIng/graphs/views.py
@@ -1,5 +1,64 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from account.models import Account
+from ad.models import Ad
+import datetime
+
+
 # Create your views here.
 class graphsTemplateView(TemplateView):
     template_name = "graphs/graphs.html"
+
+    def get_context_data(self, **kwargs):
+        year=datetime.datetime.now().year
+        context = super().get_context_data(**kwargs)
+        context['active_users'] =  Account.objects.filter(is_active=True).count()
+        context['inactive_users'] =  Account.objects.filter(is_active=False).count()
+        context['active_ads'] = Ad.objects.filter(active= True).count()
+        context['inactive_ads'] = Ad.objects.filter(active= False).count()
+
+
+        all_join_dates = Account.objects.all().filter(date_joined__year=year)
+
+        #Users*actualyear
+        ene,feb,mar,abr,may,jun,jul,ago,sep,oc,nov,dic=0,0,0,0,0,0,0,0,0,0,0,0
+
+        for date in all_join_dates:
+            if date.date_joined.month == 1:
+                en=en+1
+            elif date.date_joined.month == 2:
+                feb= feb+1
+            elif date.date_joined.month == 3:
+                mar= mar+1
+            elif date.date_joined.month == 4:
+                abr= abr+1
+            elif date.date_joined.month == 5:
+                may= may+1
+            elif date.date_joined.month == 6:
+                jun= jun+1
+            elif date.date_joined.month == 7:
+                jul= jul+1
+            elif date.date_joined.month == 8:
+                ago= ago+1
+            elif date.date_joined.month == 9:
+                sep= sep+1
+            elif date.date_joined.month == 10:
+                oc= oc+1
+            elif date.date_joined.month == 11:
+                nov= nov+1
+            elif date.date_joined.month == 12:
+                dic= dic+1   
+       
+        context['ene']=ene
+        context['feb']=feb
+        context['mar']=mar
+        context['abr']=abr
+        context['may']=may
+        context['jun']=jun
+        context['jul']=jul
+        context['ago']=ago
+        context['sep']=sep
+        context['oc']=oc
+        context['nov']=nov
+        context['dic']=dic
+        return context
