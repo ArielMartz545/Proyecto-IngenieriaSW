95c8d2b8b321b172b1373e1be7301a0b9cd01279
jarjar-st <37054631+jarjar-st@users.noreply.github.com>
2020-05-13 22:23:22 -0600

    Tiendas Favorito

--- ProyectoIng/favorites/models.py
+++ ProyectoIng/favorites/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from account.models import Account
+from store.models import Store
 # Create your models here.
 class Favorites(models.Model):
     id_user= models.ForeignKey(Account, on_delete=models.CASCADE)
@@ -12,3 +13,14 @@ class Favorites(models.Model):
     def __str__(self):
         return "Usuario: " + self.id_user.first_name +" Favorito: " + self.id_favorite_user.first_name
 
+
+class Favorites_Store(models.Model):
+    id_user= models.ForeignKey(Account, on_delete=models.CASCADE)
+    id_favorite_store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name = "favoritos_tiendas" )
+    
+    class Meta():
+        verbose_name= "Tienda Favorito"
+        verbose_name_plural= "Tiendas Favoritos"
+
+    def __str__(self):
+        return "Usuario: " + self.id_user.first_name +" Favorito: " + self.id_favorite_store.store_name
--- ProyectoIng/favorites/views.py
+++ ProyectoIng/favorites/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, HttpResponse , HttpResponseRedirect
 from django.urls import reverse_lazy
-from .models import Favorites
+from .models import Favorites , Favorites_Store
 from account.models import Account
+from store.models import Store
 
 # Create your views here.
 def ad_favorite(request):
@@ -22,3 +23,23 @@ def ad_favorite(request):
         favorite.save()
     
     return HttpResponseRedirect(reverse_lazy('profile', kwargs={'pk': fav_user_pk}))
+
+
+def ad_favorite_store(request):
+    fav_store_pk = request.POST.get('fav_store')
+    try:
+        favorite = Favorites_Store.objects.get(id_user__pk = request.user.pk, id_favorite_store__pk = fav_store_pk)
+    except:
+        favorite = None
+    if favorite is not None:
+        #Borrar de favoritos
+        favorite.delete()
+    else:
+        #Agregar a favortos
+        fav_store = Store.objects.get(pk=fav_store_pk)
+        favorite = Favorites_Store()
+        favorite.id_user = request.user
+        favorite.id_favorite_store = fav_store
+        favorite.save()
+    
+    return HttpResponseRedirect(reverse_lazy('store_detail', kwargs={'pk': fav_store_pk}))
