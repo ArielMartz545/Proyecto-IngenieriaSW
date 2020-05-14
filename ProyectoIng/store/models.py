2e17bfb5c370fc8d661911bb46c4bba1057c38ea
Alexis <Alexismatamoros1996888@gmail.com>
2020-05-13 19:35:26 -0600

    Decotator is super user on gorahdpps

--- ProyectoIng/store/models.py
+++ ProyectoIng/store/models.py
@@ -13,6 +13,9 @@ class Store(models.Model):
     store_profile_img= models.ForeignKey(Image, on_delete=models.CASCADE,related_name="store_profile_img", default="1", null=True)
     store_cover_img= models.ForeignKey(Image, on_delete=models.CASCADE,related_name="store_cover_img",default="1", null=True)
     date_created =models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Creacion')
+    store_phone_number = models.CharField(verbose_name='Número de teléfono', max_length=20, null=True, blank=True, default = None)
+    store_website = models.CharField(verbose_name='Sitio web', max_length=60, null=True, blank=True, default = None)
+    store_email = models.EmailField(verbose_name='Correo electrónico', max_length=100, null=True, blank=True, default = None)
     #Booleano para saber si la tienda esta activa o no (Borrada o no)
     active= models.BooleanField(default=True, verbose_name='Activa')
 
