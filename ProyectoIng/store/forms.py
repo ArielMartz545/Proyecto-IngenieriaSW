514d3f92c3cfcd047df24324b6ece76ebc4471c0
fabricio <fabriciomurillo24@gmail.com>
2020-05-13 19:17:06 -0600

    Editar datos de tienda
    Se agregaron campos de telefono, email, y website para agregar a una tienda.

--- ProyectoIng/store/forms.py
+++ ProyectoIng/store/forms.py
@@ -12,18 +12,15 @@ class StoreUpdateForm(forms.ModelForm):
     class Meta():
         model = Store
         #Campos a actualizar en el formulario
-        fields=('store_name', 'store_description', 'store_location')
+        fields=('store_name', 'store_description', 'store_location', 'store_phone_number', 'store_website', 'store_email')
         #Nuevos labels para los campos, los que se muestran en el HTML al usuario
         labels = {
             'store_name':'Nombre de la tienda',
             'store_description':'Descripción de la tienda',
-            'store_location':'Ubicación de la tienda'
-        }
-        #Campos que iran en el formulario
-        widgets = {
-            'store_name': forms.TextInput (attrs={'class':'form-control'}),
-            'store_description': forms.Textarea (attrs={'class':'form-control'}),
-            'store_location': forms.Select(choices="Ubicacion",attrs={'class': 'form-control'}),
+            'store_location':'Ubicación de la tienda',
+            'store_phone_number':'Número de teléfono', 
+            'store_website':'Sitio web', 
+            'store_email':'Correo electrónico',
         }
 
 #Eliminando una tienda
